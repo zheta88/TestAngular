acb36ae396fe41c62162a2078e3f0c894007b23e
Zully Tamayo M <52215922+zheta88@users.noreply.github.com>
2022-04-11 07:33:06 -0500

    modal,tabla, checkboxes

--- ProyectoAngular/src/app/app.module.ts
+++ ProyectoAngular/src/app/app.module.ts
@@ -1,18 +1,39 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import {MaterialExampleModule} from '../material.module';
+import { DataTablesModule } from "angular-datatables";
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EntidadesComponent } from './entidades/entidades.component';
+import { HttpClientModule } from "@angular/common/http";
+import {NgxPaginationModule} from 'ngx-pagination'; 
+import {Modal} from './entidades/entidades.component';
+import {MatDialogModule} from '@angular/material/dialog';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    EntidadesComponent
+    EntidadesComponent,
+    Modal
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    DataTablesModule,
+    HttpClientModule,
+    NgxPaginationModule,
+    MatDialogModule,
+    MaterialExampleModule,
+    BrowserAnimationsModule,
+    MatCheckboxModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ProyectoAngular/src/app/entidades/entidades.component.ts
+++ ProyectoAngular/src/app/entidades/entidades.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { EntidadesService } from '../entidades.service';
+import {MatDialog} from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-entidades',
@@ -6,10 +9,67 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./entidades.component.css']
 })
 export class EntidadesComponent implements OnInit {
+  title = 'Seleccione la(s) entidades que desee consultar:';
+  masterSelected:boolean;
+  checkedList:any;
+  entidadesSeleccionadas: Array<any> = [];
+  entidades: any;
+  id:any;
+  page:any;
+  
 
-  constructor() { }
-
-  ngOnInit(): void {
+    constructor(public EntidadesService: EntidadesService,public dialog: MatDialog) { 
+    this.masterSelected = false;
+    this.getCheckedItemList();
+  }
+  checkUncheckAll() {
+    for (var i = 0; i < this.entidadesSeleccionadas.length; i++) {
+      this.entidadesSeleccionadas[i].isSelected = this.masterSelected;
+    }
+    this.getCheckedItemList();
   }
 
+  isAllSelected() {
+    this.masterSelected = this.entidadesSeleccionadas.every(function(item:any) {
+        return item.isSelected == true;
+      })
+    this.getCheckedItemList();
+  }
+
+  getCheckedItemList(){
+    this.checkedList = [];
+    for (var i = 0; i < this.entidadesSeleccionadas.length; i++) {
+      if(this.entidadesSeleccionadas[i].isSelected)
+      this.checkedList.push(this.entidadesSeleccionadas[i]);
+    }
+    console.log(this.checkedList)
+  }
+
+  ngOnInit() {
+    this.EntidadesService.getEntidad().subscribe(data=>{
+      this.entidades = data;
+      this.page="";
+      for(let elem of data.data){
+        const obj:any = 
+        {
+        'id': elem['id'],
+        'email': elem['email'],
+        'first_name': elem['first_name'],
+        'last_name': elem['last_name'],
+        'avatar' : elem['avatar'],
+        'checked': false}
+        this.entidadesSeleccionadas.push(obj);
+      }
+    });
+  }
+
+  Delete():void{
+  }
+ 
+}
+@Component({
+  selector: 'modal',
+  templateUrl: 'modal.html',
+})
+export class Modal {
 }
