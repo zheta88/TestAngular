345943627ad8e9adb5f54bb6e663caa197d5504a
Zully Tamayo M <52215922+zheta88@users.noreply.github.com>
2022-04-10 20:27:40 -0500

    Creación componente Entidades

--- ProyectoAngular/src/app/app.module.ts
+++ ProyectoAngular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EntidadesComponent } from './entidades/entidades.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EntidadesComponent
   ],
   imports: [
     BrowserModule,
